c35be324726720c5ca0f36610bd253ad5134b9a3
choakly <oakley1217@gmail.com>
2025-02-26 11:00:36 -0500

    Re-push for AnimateOnHandInput script
    Added some comments in the AnimateOnHandScript

--- Assets/AnimateHandOnInput.cs
+++ Assets/AnimateHandOnInput.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class AnimateHandOnInput : MonoBehaviour
 {
+    //Unity XRI stuff, basically the "variable" is an action, more class types rather than data types (i think)
     public InputActionProperty pinchAnimationAction;
     public InputActionProperty gripAnimationAction;
     public Animator handAnimator;
@@ -14,6 +15,10 @@ public class AnimateHandOnInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // for both the trigger and grip, they have floats (bc they're on axes), but we read the value, 
+        // and the animator for the hand assigns it to either the trigger or grip, 
+        // and then in they're BlendTrees (idk) assigns that read value. Kind of stagnant but it works for our purposes of VR looking stuff
+        // Can also use these for when we want game stuff to happen ex: if triggerValue > 0.1 { shoot() }
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
         handAnimator.SetFloat("Trigger",triggerValue);
 
