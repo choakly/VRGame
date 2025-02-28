8ba1ede70bf4be112d72206ee8ecec04659a889b
choakly <oakley1217@gmail.com>
2025-02-28 12:35:01 -0500

    Gun and Attach Point
    Tutorial had a gun thing for it which is awesome so we dont even need to code the shooting part. Also taugh attach points, so we can make two hand weapons and have both hands attach at certain points

---
+++ Assets/FireBulletOnActivate.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBulletOnActivate : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+    }
+}
