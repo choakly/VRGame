d4809040f64eafa7a2d06cdf0a647b3f34c4b102
choakly <oakley1217@gmail.com>
2025-03-07 17:16:17 -0500

    Reload + Highlight
    Basic Relo9ad function thing, currently definitely works as a righty not a lefty, may know why but not super sure. Also made it so u can see if you are hovering i, mainly for testing but also QoL, as it can be finicky sometimes and idk why

--- Assets/FireBulletOnActivate.cs
+++ Assets/FireBulletOnActivate.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+
 
 public class FireBulletOnActivate : MonoBehaviour
 {
@@ -7,23 +9,54 @@ public class FireBulletOnActivate : MonoBehaviour
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed = 20;
+    public float ammo = 8;
+    protected bool thing = false;
+    public float maxAmmo = 8;
+    public InputActionReference reloadButton;
     void Start()
     {
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        XRGrabInteractableTwoAttached hoverable = GetComponent<XRGrabInteractableTwoAttached>();
+        hoverable.hoverEntered.AddListener(HoverEnteredThing);
+        hoverable.hoverExited.AddListener(HoverExitedThing);
         grabbable.activated.AddListener(FireBullet);
+        reloadButton.action.started += Reload;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     public void FireBullet(ActivateEventArgs arg)
     {
-        GameObject spawnedBullet = Instantiate(bullet);
-        spawnedBullet.transform.position = spawnPoint.position;
-        spawnedBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * fireSpeed;
-        Destroy(spawnedBullet, 5);
+        if(ammo > 0)
+        {
+            GameObject spawnedBullet = Instantiate(bullet);
+            spawnedBullet.transform.position = spawnPoint.position;
+            spawnedBullet.GetComponent<Rigidbody>().linearVelocity = spawnPoint.forward * fireSpeed;
+            Destroy(spawnedBullet, 5);
+            ammo--;
+        }
     }
+        
+    void HoverEnteredThing(HoverEnterEventArgs arg)
+    {
+        thing = true;
+    }
+    void HoverExitedThing(HoverExitEventArgs arg)
+    {
+        thing = false;
+    }
+    void Reload(InputAction.CallbackContext context)
+    {
+        if(ammo < 8 && thing == true)
+        {
+            ammo = maxAmmo;
+        }
+    }
+    
+    
 }
