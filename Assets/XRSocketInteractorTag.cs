744d10352a9d7a854dd733f04a5a2716762f4b59
choakly <oakley1217@gmail.com>
2025-03-28 11:33:35 -0400

    u can put a clip in a pull it out
    Very buggy still, as there's complications with how the clip and interactions works, and they can sometimes both try to grab. Also the clip is just way offcenter from the hand

---
+++ Assets/XRSocketInteractorTag.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSocketInteractorTag : XRSocketInteractor
+{
+    public string targetTag;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.CompareTag(targetTag);
+    }
+}
