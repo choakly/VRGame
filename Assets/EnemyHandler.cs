8c9f0cebf294d716a32c79a5605e6db249a89c72
Jason Vito <vitojason456@gmail.com>
2025-04-09 13:43:11 -0400

    Test

--- Assets/EnemyHandler.cs
+++ Assets/EnemyHandler.cs
@@ -24,6 +24,7 @@ public class EnemyHandler : MonoBehaviour
     public float killDelay = 2;
     public float killTimer;
 
+    //This is a start
     void Start()
     {
         health = maxHealth;
