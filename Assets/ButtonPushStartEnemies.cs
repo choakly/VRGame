344c966a8a7db9243b2f8bfecc98bd6a608c52fd
choakly <oakley1217@gmail.com>
2025-04-29 15:19:23 -0400

    makeshift button

---
+++ Assets/ButtonPushStartEnemies.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.DedicatedServer;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonPushStartEnemies : MonoBehaviour
+{
+    public EnemySpawner enemySpawner;
+    public GameObject buttonStand;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<XRSimpleInteractable>().selectEntered.AddListener(x => ToggleEnemySpawn());
+        
+    }
+
+    public void ToggleEnemySpawn()
+    {
+        enemySpawner.enabled = true;
+        buttonStand.SetActive(false);
+    }
+
+    // Update is called once per frame
+    
+}
